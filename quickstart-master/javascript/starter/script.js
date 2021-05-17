8e1e6eff6c20ffdea2b1e4b6077a22f5ffb5e075
Eddy Guo <eddyyanruguo@gmail.com>
2021-05-17 11:36:37 -0400

    Prisma quickstart

---
+++ quickstart-master/javascript/starter/script.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+  // ... you will write your Prisma Client queries here
+  async function main() {
+    const post = await prisma.post.update({
+      where: { id: 2 },
+      data: { published: true },
+    })
+    console.log(post)
+  }
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
